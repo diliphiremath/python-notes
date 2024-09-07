a1636e3ef9d5495e8d29b21f148b8fcd520f08e6
Dilip Hiremath <45544469+diliphiremath@users.noreply.github.com>
2024-09-07 17:29:52 +0530

    basic syntax

---
+++ Basics/01_basic_syntax.py
@@ -0,0 +1,9 @@
+'''
+Modes of python programming
+a. Interactive mode programming: Invoke the python interpreter from command line 
+b. script mode programming: invoke the python interpreter with a script parameter
+    which begins execution of script and continues untill script is finished
+'''
+
+# hello world program
+print("Hello, World!")
